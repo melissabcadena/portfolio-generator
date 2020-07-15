a0bba0e902f85b466d2602eea620c9c245e81d9d
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-07-15 13:29:59 -0500

    add question prompt

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const fs = require('fs');
-const generatePage = require('./page-template.js')
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+    
+// const fs = require('fs');
+// const generatePage = require('./page-template.js')
 
-fs.writeFile('./index.html', generatePage(name,github), err => {
-    if (err) throw new Error(err);
+// const profileDataArgs = process.argv.slice(2);
+// const [name, github] = profileDataArgs;
 
-    console.log('Portfolio complete! Check out index.html to see the output!')
-})
+// fs.writeFile('./index.html', generatePage(name,github), err => {
+//     if (err) throw new Error(err);
+
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// })
